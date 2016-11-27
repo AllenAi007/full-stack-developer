db074ed372ad6d7dd93551db6989b8e85ac5a781
hua.ai <13348867336@163.com>
2016-11-27 23:10:12 +0800

    init version

---
+++ spring-thymeleaf/src/main/java/org/ai/AiSpringThymeleafApplication.java
@@ -0,0 +1,20 @@
+package org.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Add a comments
+ */
+@SpringBootApplication
+public class AiSpringThymeleafApplication {
+
+	/**
+	 * Add comments
+	 * @param args
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(AiSpringThymeleafApplication.class, args);
+	}
+
+}
